f5fb130e21da7f5431f85319c9a93c4a9d59760e
FrozenPandaz <jasonjean1993@gmail.com>
2025-07-14 13:21:50 -0400

    feat: update android frontend build config and nx settings

--- apps/android-frontend/build.gradle.kts
+++ apps/android-frontend/build.gradle.kts
@@ -4,4 +4,11 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     id("com.google.dagger.hilt.android") version "2.56.2" apply false
+    id("dev.nx.gradle.project-graph") version "0.1.0"
+}
+
+allprojects {
+    apply {
+        plugin("dev.nx.gradle.project-graph")
+    }
 }
